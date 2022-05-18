d059bc4174c6f87092b3c1c3ceecffb10dd5716f
David Pesta <davidpesta@gmail.com>
2022-05-17 20:06:52 -0500

    Added unit test for the sleep utility.

---
+++ tests/sleep_test.ts
@@ -0,0 +1,10 @@
+import easy from "../mod.ts";
+import { assert } from "https://deno.land/std@0.139.0/testing/asserts.ts";
+
+Deno.test("easy.sleep", async () => {
+	const beforeTime = Date.now();
+	await easy.sleep(50);
+	const afterTime = Date.now();
+	assert(afterTime - beforeTime >= 50);
+	assert(afterTime - beforeTime < 60);
+});
