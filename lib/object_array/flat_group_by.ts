a2b24b1980c57a428b4459d9ad832f6ee9d4e258
David Pesta <davidpesta@gmail.com>
2022-08-14 11:25:19 -0500

    Added notes to show preferred code solutions that don't work.

--- lib/object_array/flat_group_by.ts
+++ lib/object_array/flat_group_by.ts
@@ -2,5 +2,6 @@ import easy from "../../mod.ts";
 
 // deno-lint-ignore no-explicit-any
 export function flatGroupBy(objArr: Array<Record<string, any>>, key: string): Record<string, Record<string, any>> {
+	// return mapValues(groupBy(objArr, x => x[key]), x => x![0]); // x[key] doesn't work
 	return easy.objectArray.convertTo.flatKeyedObjects(objArr, key);
 }
--- lib/object_array/group_by.ts
+++ lib/object_array/group_by.ts
@@ -2,5 +2,6 @@ import easy from "../../mod.ts";
 
 // deno-lint-ignore no-explicit-any
 export function groupBy(objArr: Array<Record<string, any>>, key: string): Record<string, Array<Record<string, any>>> {
+	// return groupBy(objArr, x => x[key]); // x[key] doesn't work
 	return easy.objectArray.convertTo.keyedObjects(objArr, key);
 }
